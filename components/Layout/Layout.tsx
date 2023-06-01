b57d6743a8db2c640c5758b072fd744b66693282
Ivan Shamshurin <shamshurin.iv@yandex.ru>
2023-06-01 15:59:08 +0300

    SITE-159: added flowbite ui-kit

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { Carousel } from 'flowbite-react'
 import { FC } from 'react'
 
 import './styles.css'
@@ -5,7 +8,30 @@ import './styles.css'
 export const MainLayout: FC = ({ children }) => {
   return (
     <div className='wrapper flex flex-col min-h-screen'>
-      <header className='page-header'>Header</header>
+      <header className='page-header'>
+        <Carousel className='h-60'>
+          <img
+            alt='...'
+            src='https://flowbite.com/docs/images/carousel/carousel-1.svg'
+          />
+          <img
+            alt='...'
+            src='https://flowbite.com/docs/images/carousel/carousel-2.svg'
+          />
+          <img
+            alt='...'
+            src='https://flowbite.com/docs/images/carousel/carousel-3.svg'
+          />
+          <img
+            alt='...'
+            src='https://flowbite.com/docs/images/carousel/carousel-4.svg'
+          />
+          <img
+            alt='...'
+            src='https://flowbite.com/docs/images/carousel/carousel-5.svg'
+          />
+        </Carousel>
+      </header>
       <main className='page-main flex-grow'>{children}</main>
       <footer className='page-footer'>Footer</footer>
     </div>
