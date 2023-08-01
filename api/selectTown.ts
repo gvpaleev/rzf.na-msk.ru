d49a7393fd1be4c2942d68a27421291aabced668
Шамшурин Иван <i.shamshurin@bankismart.com>
2023-08-01 22:19:02 +0300

    SITE-108: get current town request

---
+++ api/selectTown.ts
@@ -0,0 +1,12 @@
+import { requestService } from '@/api/requestService'
+
+type SelectTownParams = { townId: number }
+
+type TownType = { id: number; name: string }
+
+export const selectTown = ({ townId }: SelectTownParams) =>
+  requestService.get<SelectTownParams, TownType>('/current_town/', {
+    params: {
+      town_id: townId,
+    },
+  })
--- components/SelectTownModal/store.ts
+++ components/SelectTownModal/store.ts
@@ -1,3 +1,20 @@
+import { selectTown } from '@/api/selectTown'
 import { createModalStore } from '@/utils/store'
+import { createEffect, createEvent, sample } from 'effector'
 
 export const selectTownModal = createModalStore()
+
+export const selectDefaultTownEvent = createEvent()
+
+const DEFAULT_TOWN_ID = 1
+
+const selectDefaultTownFx = createEffect(() => {
+  console.log('>>>>>>>>>>>>>>>> select town')
+  return selectTown({ townId: DEFAULT_TOWN_ID })
+})
+
+sample({ clock: selectDefaultTownEvent, target: selectDefaultTownFx })
+
+selectDefaultTownFx.done.watch((town) => {
+  console.log('>>>>>>>>>>>>>>>> town', town)
+})
