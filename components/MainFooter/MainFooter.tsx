3850766287e8df4b36e14626b3a5d9b2e441647b
Андрей Терещенко <andrejteresenko@192.168.1.16>
2023-09-25 21:24:29 +0300

    SITE-181 mobile footer design

--- components/MainFooter/MainFooter.tsx
+++ components/MainFooter/MainFooter.tsx
@@ -1,23 +1,34 @@
 import Image from 'next/image'
 import { PhoneBlock } from '../PhoneBlock'
 import { Menu } from '../Menu'
+import { RegionButton } from '../Layout/RegionButton'
+import { useEvent, useStore } from 'effector-react'
+import { $currentTown } from '../SelectTownModal/store/currentTown'
+import { selectTownModal } from '../SelectTownModal/store'
 
-export const MainFooter = () => (
-  <div className="bg-primary-grey px-22 py-20 mt-10">
-    <div className='flex flex-row w-full lg:w-128 mx-auto gap-x-10 flex-grow items-start justify-between'>
-      <PhoneBlock />
+export const MainFooter = () => {
+  const currentTown = useStore($currentTown)
+  const openModal = useEvent(selectTownModal.openModalEvent)
 
-      <Menu />
-    </div>
+  return (
+    <div className="bg-primary-grey px-22 py-2 max-lg:py-10 mt-10">
+      <div className='flex flex-row w-full lg:w-128 mx-auto gap-x-10 flex-grow items-start justify-between max-lg:justify-center'>
+        <PhoneBlock />
 
-    <div className='flex flex-row w-full pt-10 lg:w-128 mx-auto flex-grow items-start justify-between'>
-      <div className='text-sm text-secondary-blue'>
-        © 2003-{new Date().getFullYear()} Официальный сайт сообщества
-        <br />
-        Анонимные Наркоманы региона Большая Москва
+        <Menu />
       </div>
 
-      <Image width="172" height="151" alt="Google Play" src="/play.svg" />
+      <div className="hidden justify-center my-5 max-lg:flex">
+        <RegionButton currentTown={currentTown?.name} onClick={openModal} />
+      </div>
+
+      <div className='flex flex-row w-full pt-10 max-lg:pt-0 lg:w-128 mx-auto flex-grow justify-between items-start max-lg:items-center max-lg:flex-col'>
+        <div className='text-sm text-secondary-blue max-lg:text-center w-96 max-lg:w-80'>
+          © 2003-{new Date().getFullYear()} Официальный сайт сообщества Анонимные Наркоманы региона Большая Москва
+        </div>
+
+        <Image width="172" height="151" alt="Google Play" src="/play.svg" className="max-lg:p-5" />
+      </div>
     </div>
-  </div>
-)
+  );
+}
--- components/WhatsappLogo/WhatsappLogo.tsx
+++ components/WhatsappLogo/WhatsappLogo.tsx
@@ -3,6 +3,6 @@ import { FC } from 'react'
 
 export const WhatsappLogo: FC<{ className?: string }> = ({ className }) => (
   <div className={className}>
-    <Image src='./whatsapp.svg' alt='...' width={38} height={38} />
+    <Image src='/whatsapp.svg' alt='...' width={38} height={38} />
   </div>
 )
