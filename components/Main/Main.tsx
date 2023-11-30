1bbca8d8c39230448ad3336faabce0118a1f970a
Mitya Fox <partyzzzan77@yandex.ru>
2023-11-30 10:18:43 +0000

    SITE-11: radio widget

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -3,6 +3,7 @@ import React, { FC } from 'react'
 import { ANInComunity, ForNewcomer, ForParticipant } from './components'
 import { Meditation } from './components/Meditation'
 import { News } from './components/News'
+import { Radio } from './components/Radio'
 
 export const Main: FC = () => {
   return (
@@ -12,11 +13,11 @@ export const Main: FC = () => {
         <ForParticipant />
         <ANInComunity />
       </div>
-
       <div className='w-full lg:gap-10 grid grid-cols-3'>
         <Meditation />
         <News />
       </div>
+      <Radio />
     </div>
   )
 }
--- components/Main/components/index.ts
+++ components/Main/components/index.ts
@@ -1,3 +1,4 @@
 export * from './ForNewcomer'
 export * from './ForParticipant'
 export * from './ANInComunity'
+export * from './Radio'
