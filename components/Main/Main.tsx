d7b3cc027685f03ad7711405525b61061e22a533
Mitya Fox <partyzzzan77@yandex.ru>
2023-12-12 15:56:17 +0000

    SITE-113: Виджет газеты

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -1,12 +1,12 @@
 import React, { FC } from 'react'
 
-import { ANInComunity, ForNewcomer, ForParticipant, Radio, SocialLink } from './components'
+import { ANInComunity, ForNewcomer, ForParticipant, Newspaper, Radio, SocialLink } from './components'
 import { Meditation } from './components/Meditation'
 import { News } from './components/News'
 
 export const Main: FC = () => {
   return (
-    <div className="flex flex-col gap-10 lg:gap-10">
+    <div className="flex flex-col lg:gap-10">
       <div className='flex flex-col lg:flex-row lg:justify-around w-full lg:gap-10'>
         <ForNewcomer />
         <ForParticipant />
@@ -17,6 +17,7 @@ export const Main: FC = () => {
         <Meditation />
       </div>
       <Radio />
+      <Newspaper/>
       <div className='w-full lg:gap-8 grid grid-cols-1 lg:grid-cols-3'>
         <SocialLink header="Литрес" text="Покупай литературу сообщества Анонимные Наркоманы в электронном формате на сайте" link="https://www.litres.ru/author/anonimnye-narkomany-32034193/" imgSrc="/litres.svg" />
         <SocialLink header="Youtube канал" text="Смотри личные истории и интервью на нашем канале в YouTube" link="https://www.youtube.com/channel/UCMD2eYn1t6Tz6eb4ShQMxYw" imgSrc="/Youtube.svg" />
