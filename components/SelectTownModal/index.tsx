07196d9b220fa48ed5bbe039e386f2d3540ae7d8
Mitya Fox <partyzzzan77@yandex.ru>
2023-12-21 19:06:05 +0000

    SITE-306: modal window

--- components/SelectTownModal/index.tsx
+++ components/SelectTownModal/index.tsx
@@ -19,16 +19,18 @@ const SelectTownModal: FC = () => {
       <Modal.Header />
       <Modal.Body>
         <div className='flex flex-col items-center'>
-          <div>
-            Ваш город <span className='font-bold'>Москва</span>?
-          </div>
-          <div className='flex flex-col w-full'>
-            <Button color='purple' onClick={selectDefaultTown}>
-              Все верно
-            </Button>
-            <Button color='gray' onClick={openTownList}>
-              Сменить город
-            </Button>
+          <div className='flex flex-col gap-10'>
+            <div className='font-thin text-2xl'>
+              Ваш город <span className='font-bold'>Москва</span>?
+            </div>
+            <div className='flex flex-col w-full gap-2.5'>
+              <Button type='button' className="!text-blue-700 hover:bg-blue-700 hover:!text-white ring-1 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={selectDefaultTown}>
+                Все верно
+              </Button>
+              <Button  type='button' className="!text-blue-700 hover:bg-blue-700 hover:!text-white ring-1 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={openTownList}>
+                Сменить город
+              </Button>
+            </div>
           </div>
         </div>
       </Modal.Body>
