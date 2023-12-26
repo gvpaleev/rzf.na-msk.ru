223356c3e5eddb224d94ebb2436b6d72e3f38dc6
Андрей Терещенко <andrejteresenko@MacBook-Air-Andrej.local>
2023-12-26 13:23:12 +0300

    SITE-323 regionalized news

--- components/Main/components/Meditation.tsx
+++ components/Main/components/Meditation.tsx
@@ -1,9 +1,5 @@
 import { FC, useEffect, useMemo, useState } from 'react'
 
-import { Colors } from '../types'
-import { MainBlock } from './MainBlock'
-
-import ReactHtmlParser from 'react-html-parser';
 import Link from 'next/link';
 
 const MONTHS = [
@@ -30,11 +26,10 @@ export const Meditation: FC = () => {
 
   const item = useMemo(() => data && {
     date: `${data.day} ${MONTHS[data.month - 1]}`,
-    title: <>{ReactHtmlParser(data.title)}</>,
-    quote: <>{ReactHtmlParser(data.quote)}</>,
-    source: <>{ReactHtmlParser(data.quote_from)}</>,
-    text: <>{ReactHtmlParser(data.body)}</>,
-    just4today: <>ТОЛЬКО СЕГОДНЯ: {ReactHtmlParser(data.jft)}</>,
+    title: data.title,
+    quote: data.quote,
+    source: data.quote_from,
+    text: `${data.body}<br/><br/>ТОЛЬКО СЕГОДНЯ: ${data.jft}`,
   }, [data]);
 
   const [expanded, setExpanded] = useState(false);
@@ -49,30 +44,19 @@ export const Meditation: FC = () => {
           {item.date}
         </div>
 
-        <div className='text-lg font-bold'>
-          {item.title}
-        </div>
+        <div className='text-lg font-bold' dangerouslySetInnerHTML={{__html: item.title}} />
 
         <div className="mt-4 flex flex-row items-end gap-10 lg:gap-20">
           <div className='flex w-full'>
-            <div className='text-md'>
-              {item.quote}
-            </div>
+            <div className='text-md' dangerouslySetInnerHTML={{__html: item.quote}} />
           </div>
 
           <div className=''>
-            <div className='text-md whitespace-nowrap text-secondary-blue'>
-              {item.source}
-            </div>
+            <div className='text-md whitespace-nowrap text-secondary-blue' dangerouslySetInnerHTML={{__html: item.source}} />
           </div>
         </div>
 
-        <div className={`mt-4 text-md ${expanded ? '' : 'line-clamp-4'} lg:line-clamp-none`}>
-          {item.text}
-          <br />
-          <br />
-          {item.just4today}
-        </div>
+        <div className={`mt-4 text-md ${expanded ? '' : 'line-clamp-4'} lg:line-clamp-none`} dangerouslySetInnerHTML={{__html: item.text}} />
 
         {!expanded && (
           <div className='block lg:hidden border-t mt-5 pt-5 border-secondary-blue'>
