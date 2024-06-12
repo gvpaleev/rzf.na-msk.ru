3935b60d77c43c80fa861e28120819c1f7d4f5c5
Your Name <you@example.com>
2024-06-12 17:39:40 +0000

    v0.0.6

--- next.config.js
+++ next.config.js
@@ -1,29 +1,40 @@
+
 module.exports = {
   images: {
-    domains: ['courses-top.ru']
+    domains: ['courses-top.ru'],
   },
   webpack(config, options) {
     config.module.rules.push({
-      loader: '@svgr/webpack',
-      issuer: /\.[jt]sx?$/,
-      options: {
-        prettier: false,
-        svgo: true,
-        svgoConfig: {
-          plugins: [{
-            name: 'preset-default',
-            params: {
-              override: {
-                removeViewBox: false
-              }
-            }
-          }],
-        },
-        titleProp: true,
-      },
       test: /\.svg$/,
+      issuer: {
+        and: [/\.[jt]sx?$/],
+      },
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            prettier: false,
+            svgo: true,
+            svgoConfig: {
+              plugins: [
+                {
+                  name: 'preset-default',
+                  params: {
+                    override: {
+                      removeViewBox: false,
+                    },
+                  },
+                },
+              ],
+            },
+            titleProp: true,
+          },
+        },
+        'url-loader',
+      ],
     });
 
     return config;
   },
-}
+};
+
