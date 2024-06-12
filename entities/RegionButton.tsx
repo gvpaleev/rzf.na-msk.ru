ba30517effa1c6c91e5e228db8c7a44cc5e7a505
Your Name <you@example.com>
2024-06-12 15:12:52 +0000

    v0.0.2

---
+++ entities/RegionButton.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+import { FC, MouseEventHandler } from 'react'
+
+export const RegionButton: FC<{
+  currentTown?: string
+  onClick?: MouseEventHandler<HTMLButtonElement>
+  className?: string
+}> = ({ currentTown, onClick, className }) => (
+  <div className={className}>
+    <button onClick={onClick} className='flex items-center'>
+      <Image
+        className='animate-spin-slow inline'
+        src={'/location.svg'}
+        alt={'...'}
+        height={23}
+        width={23}
+      />
+      <span className='font-medium text-lg font-bold text-primary-blue pl-2'>
+        {currentTown}
+      </span>
+    </button>
+  </div>
+)
