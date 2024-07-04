f75a6c1b6a89beefa03e59065333730f76673d3c
Gennady Vladimirovich <gv.paleev@gmail.com>
2024-07-04 22:45:25 +0000

    v0.1.0

--- Widgets/ConfirmTownModal/ConfirmTownModal.tsx
+++ Widgets/ConfirmTownModal/ConfirmTownModal.tsx
@@ -7,52 +7,55 @@ import { useUnit } from "effector-react";
 import { CityList } from "@/shared/contract/CityList";
 import { getCityList } from "@/api/getCityList";
 import { useEffect } from "react";
-import { $townId } from "@/shared/state/userAppState";
+import { $regionId, $regionName, $townId, $townName } from "@/shared/state/userAppState";
+import { spread } from "patronum";
 
-// const isTown = useUnit($townId) !== -1;
 
-const isTown = $townId.getState() !== -1;
 
 export const opendModalEvent = createEvent();
 export const closedModalEvent = createEvent();
+export const $isOpen = createStore<boolean>(true)
+  .on(opendModalEvent, () => true)   // Обновляем стор при вызове события открытия
+  .on(closedModalEvent, () => false);
+
 
 const loadCityListRx = createEffect(async () => {
   let data = await getCityList();
   return data;
+
 })
-
-export const $isOpen = createStore<boolean>(!isTown)
-  .on(opendModalEvent, () => true)   // Обновляем стор при вызове события открытия
-  .on(closedModalEvent, () => false);
-
-const $cityList = createStore<CityList>({ big_towns: [], towns: [], regions: [] })
-  .on(loadCityListRx.doneData, (_, cityList) => cityList)
 
 sample({
-  clock: loadCityListRx.doneData,
+  source: loadCityListRx.doneData,
+  fn: (data) => {
+    let { id: townId, name: townName, geographic_region: regionId } = data.towns.filter((item) => item.name == 'Москва')[0];
+    let { name: regionName } = data.regions.filter((item) => item.id == regionId)[0];
 
-})
-
+    return { townId, townName, regionId, regionName };
+  },
+  target: spread({
+    townId: $townId, townName: $townName, regionId: $regionId, regionName: $regionName
+  }),
+});
 
 export function ConfirmTownModal() {
   const isOpen = useUnit($isOpen)
   const onClose = useUnit(closedModalEvent)
-  const cityList = useUnit($cityList)
   const loadCityList = useUnit(loadCityListRx)
+  const townName = useUnit($townName);
+
   useEffect(() => {
-    loadCityList();
+    if (townName === '')
+      loadCityList();
   }, [])
   const accept = () => {
-    // cityList.towns
-    console.log(cityList);
-    debugger;
   }
   return (
-    <ModalWrapper isOpen={isOpen} onClose={isTown ? onClose : () => { }}>
+    <ModalWrapper isOpen={isOpen} onClose={onClose}>
       <div className={classNames(styles.item)}>
 
-        <h1 className={classNames(styles.h1)} >Ваш город <b>Москва</b> ?</h1>
-        <button className={classNames(styles.accept, styles.button)} onClick={accept}>Все верно</button>
+        <h1 className={classNames(styles.h1)} >Ваш город <b>{townName}</b> ?</h1>
+        <button className={classNames(styles.accept, styles.button)} onClick={onClose}>Все верно</button>
         <button className={classNames(styles.cancel, styles.button)}>Сменить город</button>
       </div>
 
--- shared/state/userAppState.tsx
+++ shared/state/userAppState.tsx
@@ -1,6 +1,31 @@
+'use client'
 import { createStore } from "effector";
+import localStorageCache from "./localStorageCache";
 
-export const $townId = createStore<number>(-1);
-export const $townName = createStore<string>('Москва')
-export const $regionId = createStore<number>(-1)
-export const $regionName = createStore<string>('Большая Москва')
+const townId = localStorageCache.getItem('townId');
+
+const townName = localStorageCache.getItem('townName');
+
+const regionId = localStorageCache.getItem('regionId');
+
+const regionName = localStorageCache.getItem('regionName')
+
+export const $townId = createStore<number>(townId === '' ? -1 : +townId);
+$townId.watch((newState) => {
+  localStorageCache.setItem('townId', `${newState}`)
+});
+
+export const $townName = createStore<string>(townName);
+$townName.watch((newState) => {
+  localStorageCache.setItem('townName', `${newState}`)
+});
+
+export const $regionId = createStore<number>(regionId === '' ? -1 : +townId);
+$regionId.watch((newState) => {
+  localStorageCache.setItem('regionId', `${newState}`)
+});
+
+export const $regionName = createStore<string>(regionName);
+$regionName.watch((newState) => {
+  localStorageCache.setItem('regionName', `${newState}`)
+});
