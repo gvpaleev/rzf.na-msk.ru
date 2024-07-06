b7fc6ed0f159c7e7df18dccd17329ff6c72fdb3c
Gennady Vladimirovich <gv.paleev@gmail.com>
2024-07-06 23:00:36 +0000

    v0.3.1

--- utils/store/tokenStore.ts
+++ utils/store/tokenStore.ts
@@ -8,6 +8,8 @@ const REFRESH_TOKEN = 'REFRESH_TOKEN'
 const refreshTokenCache = createCacheStoreForKey(REFRESH_TOKEN)
 
 export const loginEvent = createEvent<LoginParams>()
+export const logoutEvent = createEvent()
+export const resetErrorEvent = createEvent()
 
 const loginFx = createEffect<LoginParams, TokenResponse>((params) =>
   login(params),
@@ -15,13 +17,14 @@ const loginFx = createEffect<LoginParams, TokenResponse>((params) =>
 
 export const $token = createStore<string | null>(null)
 
-$token.on(loginFx.doneData, (_, { access }) => access)
+$token.on(loginFx.doneData, (_, { access }) => access).reset(logoutEvent)
 
 export const $loginError = createStore<string | null>(null)
 
 $loginError
   .on(loginFx.done, () => null)
   .on(loginFx.fail, (_, { error }) => error.message)
+  .reset([logoutEvent, resetErrorEvent])
 
 sample({
   clock: loginEvent,
