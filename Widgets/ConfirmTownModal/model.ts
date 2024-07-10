43a8a6b7e13bfdc58bfea48553c80df367a504c7
Gennady Vladimirovich <gv.paleev@gmail.com>
2024-07-10 21:10:32 +0000

    v0.1.0

--- Widgets/ConfirmTownModal/model.ts
+++ Widgets/ConfirmTownModal/model.ts
@@ -1,6 +1,8 @@
 import { getCityList } from "@/api/getCityList";
 import { $currentTown } from "@/components/SelectTownModal/store/currentTown";
 import { $regionId, $regionName, $townId, $townName } from "@/shared/state/userAppState";
+import { getItemFromLocalStorage } from "@/utils/getItemFromLocalStorage";
+import { LocalStorageKeys } from "@/utils/types/utils";
 import { createEffect, createEvent, createStore, sample } from "effector";
 import { spread } from "patronum";
 
@@ -19,12 +21,24 @@ export const loadCityListFx = createEffect(async () => {
 sample({
   clock: loadCityListFx.doneData,
   fn: (data) => {
-    let { id: townId, name: townName, geographic_region: regionId } = data.towns.filter((item) => item.name == 'Москва')[0];
+    let townCache = {
+      townId: getItemFromLocalStorage<string>(LocalStorageKeys.TOWN_ID) || '-1',
+      townName: getItemFromLocalStorage<string>(LocalStorageKeys.TOWN_NAME) || '',
+      regionId: getItemFromLocalStorage<string>(LocalStorageKeys.REGION_ID) || '-1',
+      regionName: getItemFromLocalStorage<string>(LocalStorageKeys.REGION_NAME) || ''
+    };
+    let town = data.towns.filter((item) => item.name == 'Москва')[0];
 
-
+    // let { id: townId, name: townName, geographic_region: regionId } = townCache.townId != -1 ? townCache : data.towns.filter((item) => item.name == 'Москва')[0];
+    let { id: townId, name: townName, geographic_region: regionId } = town;
     let { name: regionName } = data.regions.filter((item) => item.id == regionId)[0];
 
-    return { townId, townName, regionId, regionName, currentTown: { 'id': townId, 'name': townName, 'geographic_region': regionId } };
+    return townCache.townName != ''
+      ? {
+        townId: +(townCache.townId), townName: townCache.townName, regionId: +(townCache.regionId), regionName: townCache.regionName,
+        currentTown: { 'id': +(townCache.townId), 'name': townCache.townName, 'geographic_region': +(townCache.regionId) }
+      }
+      : { townId, townName, regionId, regionName, currentTown: { 'id': townId, 'name': townName, 'geographic_region': regionId } };
   },
   target: spread({
     townId: $townId,
