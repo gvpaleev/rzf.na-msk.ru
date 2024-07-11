c2dd4f36b45cb1f0745dfb3d41d6ff5a365ed78e
Gennady Vladimirovich <gv.paleev@gmail.com>
2024-07-11 21:58:14 +0000

    update-dataFlow v2

--- shared/state/userAppState.ts
+++ shared/state/userAppState.ts
@@ -1,22 +1,6 @@
 import { getItemFromLocalStorage, setItemToLocalStorage } from "@/shared/utils/getItemFromLocalStorage";
 import { LocalStorageKeys } from "@/shared/utils/types/utils";
 import { createEffect, createStore, sample } from "effector";
-import { spread } from "patronum";
-
-// export const installStateFx = createEffect(async () => {
-//   await new Promise((resolve) => {
-//     setTimeout(() => {
-//       resolve('');
-//     }, 2000);
-//   });
-//   let data = {
-//     townId: +(getItemFromLocalStorage<string>(LocalStorageKeys.TOWN_ID) || -1),
-//     townName: getItemFromLocalStorage<string>(LocalStorageKeys.TOWN_NAME) || '',
-//     regionId: +(getItemFromLocalStorage<string>(LocalStorageKeys.REGION_ID) || -1),
-//     regionName: getItemFromLocalStorage<string>(LocalStorageKeys.REGION_NAME) || ''
-//   };
-//   return data;
-// })
 export const $townId = createStore<number>(-1);
 $townId.watch((newState) => {
   if (newState == -1) return
